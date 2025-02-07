3900977a78b5b9080f6befe3bc573aa674543371
mugabiBenjamin <mugabiben6@gmail.com>
2025-02-07 20:14:10 +0300

    Add tailwind in project

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 class="text-3xl font-bold text-blue-900 text-center my-5">Hello World</h1>
     </div>
   )
 }
